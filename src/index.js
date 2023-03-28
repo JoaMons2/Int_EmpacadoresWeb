a2812b7ec3262d4d77bac70ff68bb20a24979275
JoaMons2 <ejoannamonserrat@gmail.com>
2023-03-28 12:41:24 -0600

    Proyecto a empaquetar

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "Monserrat Espinoza",
+    nickname: "JoaMons2",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/JoannaMonserr17",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://www.instagram.com/monse.espinoza.sanchez/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://github.com/JoaMons2",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://github.com/JoaMons2",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Colombia",
+  };
